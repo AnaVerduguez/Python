49aa2e532c5f1ad133d0eb11cd39ef2267e70065
Ana <64393490+AnaVerduguez@users.noreply.github.com>
2020-08-13 17:24:36 -0300

    Add files via upload

---
+++ condicionales.py
@@ -0,0 +1,18 @@
+#Condicionales
+
+x= 100
+
+if x > 50:
+ print("x es mayor a 50") #se muestra solo si es verdadero (Mayor a 50)
+else:
+ print("x es menor a 50") #se muestra solo si es falso (Menor a 50)
+
+
+color = "verde" 
+
+if color == "verde": #si
+ print("El color es verde")
+elif color == "azul": #de lo contrario si
+ print("El color es azul")
+else: #de lo contrario
+ print("Cualquier color")
